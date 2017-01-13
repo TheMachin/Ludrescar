4e6fff6c500d1a05100bc05cb083c86f149ed3d0
kevin <34araba@gmail.com>
2017-01-13 14:21:43 +0100

    delete important file
    file important

--- bdd/bdd.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/* 
- * File to connect at database PostgreSql
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-$bdd=pg_connect("host=localhost port=5432 dbname=ludrescar user=ludrescar password=ludrescar");
